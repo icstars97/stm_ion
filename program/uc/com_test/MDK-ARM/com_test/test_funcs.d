dc7790eee1dbf71128f4b179e6f1f90021186de8
icstars <and15rei@gmail.com>
2019-09-06 09:26:34 +0300

    usb fs

--- program/uc/com_test/MDK-ARM/com_test/test_funcs.d
+++
@@ -1,4 +0,0 @@
-com_test\test_funcs.o: test_funcs.c
-com_test\test_funcs.o: test_funcs.h
-com_test\test_funcs.o: I:\Program\uvision5\ARM\ARMCC\Bin\..\include\stdint.h
-com_test\test_funcs.o: I:\Program\uvision5\ARM\ARMCC\Bin\..\include\stdlib.h
